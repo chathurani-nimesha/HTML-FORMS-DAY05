c66e99be745a3f91d700108535c4563b364daff0
Chathurani Nimesha <chathunimesha389@gmail.com>
2026-06-27 13:01:41 +0530

    js variables

--- js/app.js
+++ js/app.js
@@ -1,1 +1,15 @@
 console.log("Hello JavaSript");
+
+let vari = "chathu";
+
+console.log(vari);
+console.log(typeof vari);
+
+vari= "Nimesha";
+
+let age=23;
+
+console.log(vari);
+console.log(age)
+
+alert("Hello Chathu");

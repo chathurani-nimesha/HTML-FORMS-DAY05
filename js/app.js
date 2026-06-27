730bd3e7bcb3c6b9fef71aa4bb82f05474b65c57
Chathurani Nimesha <chathunimesha389@gmail.com>
2026-06-27 14:26:16 +0530

    js Variables

--- js/app.js
+++ js/app.js
@@ -1,15 +1,24 @@
-console.log("Hello JavaSript");
+//console.log("Hello JavaSript");
+//let vari = "chathu";
+//console.log(vari);
+//console.log(typeof vari);
+//vari= "Nimesha";
+//let age=23;
+//console.log(vari);
+//console.log(age)
+//console.log(typeof age)
+//alert("Hello Chathu");
 
-let vari = "chathu";
-
-console.log(vari);
-console.log(typeof vari);
-
-vari= "Nimesha";
-
+let name="Chathu"
 let age=23;
+let isAfterAl=true;
+let customers=[]; // Array
+let customer ={};  //object
+let salary =7500.0;
 
-console.log(vari);
-console.log(age)
-
-alert("Hello Chathu");
+console.log(typeof name);
+console.log(typeof age);
+console.log(typeof isAfterAl);
+console.log(typeof customers);
+console.log(typeof customer);
+console.log(typeof salary);
